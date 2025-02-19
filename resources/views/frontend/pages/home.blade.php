08f95d2390c2a4753f382ce4a48aa1278cbe1c3d
Muhammad Hamza <m.hamza7265@gmail.com>
2025-02-19 21:28:26 +0500

    first commit

---
+++ resources/views/frontend/pages/home.blade.php
@@ -0,0 +1,34 @@
+@extends('frontend.layouts.main')
+
+@section('title', 'home page')
+
+@section('styles')
+    <style>
+        .text{
+            color: blue;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Home Page</h1>
+                <p>This is home page</p>
+                <p class="text">Home page is the first page that users see when they visit a website. It
+                    is the main entry point for users and is often the most visited page on a
+                    website.
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.text').css('color', 'red');
+        })
+    </script>
+@endsection
