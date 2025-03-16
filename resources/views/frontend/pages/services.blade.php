1fbb2abbd1c307d7814da658f00e1236e2401880
Muhammad Hamza <m.hamza7265@gmail.com>
2025-03-16 18:04:23 +0500

    empower section in services page complete

--- resources/views/frontend/pages/services.blade.php
+++ resources/views/frontend/pages/services.blade.php
@@ -15,13 +15,61 @@
         </section>
         <section class="nav-section">
             <div class="d-flex justify-content-between align-items-center px-5 py-5">
-                <a href="#" class="page-nav-link">Empowering Business</a>
-                <a href="#" class="page-nav-link">Transformation Journey</a>
-                <a href="#" class="page-nav-link">Consulting & Strategy</a>
-                <a href="#" class="page-nav-link">Transformation</a>
-                <a href="#" class="page-nav-link">Business Applications</a>
-                <a href="#" class="page-nav-link">eCommerce</a>
+                <a href="#empower" class="page-nav-link fs-16 fw-600">Empowering Business</a>
+                <a href="#" class="page-nav-link fs-16 fw-600">Transformation Journey</a>
+                <a href="#" class="page-nav-link fs-16 fw-600">Consulting & Strategy</a>
+                <a href="#" class="page-nav-link fs-16 fw-600">Transformation</a>
+                <a href="#" class="page-nav-link fs-16 fw-600">Business Applications</a>
+                <a href="#" class="page-nav-link fs-16 fw-600">eCommerce</a>
+            </div>
+        </section>
+        <section id="empower" class="empowering-section bg-white">
+            <div class="container-sm">
+                <div class="row justify-content-between align-items-start">
+                    <div id="fixed-on-scroll" class="col-12 col-md-6">
+                        <div class="text-content fixed-on-scroll">
+                            <h1 class="empower-title">
+                                Empowering Businesses to Transform & Grow
+                                <span class="gradient-band band-1"></span>
+                                <span class="gradient-band band-2"></span>
+                                <span class="gradient-band band-3"></span>
+                            </h1>
+                            
+                            <p class="empower-subtitle fs-16">We deliver innovative digital strategies, business applications, and eCommerce solutions to help you scale your organization seamlessly in today’s digital-first world</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 empower-right-texts-parent">
+                        <div class="empower-right-texts">
+                            <h5 class="fs-22 fw-500">Innovative IT Services</h5>
+                            <p>Unlock potential with cutting-edge technology tailored to your business needs. Our solutions streamline operations, enhance security, and foster innovation, positioning you for success in a rapidly evolving digital landscape.</p>
+                            <div id="div-end-unfiix"></div>
+                        </div>
+                        
+                        <div class="empower-right-texts">
+                            <h5>Strategic Industry Insights</h5>
+                            <p>Gain a competitive edge with our in-depth analysis of market trends and emerging technologies. Our experts provide actionable insights that empower your strategic decisions and drive sustainable growth.</p>
+                        </div>
+
+                        <div class="empower-right-texts">
+                            <h5>Transformative Digital Experiences</h5>
+                            <p>Experience digital transformation that engages customers and revolutionizes your operations. We create seamless, user-centric digital interfaces that improve engagement and optimize user experience.</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </section>
     </div>
+@endsection
+@section('scripts')
+    <script>
+       $(window).on("scroll", function () {
+            $(window).on("scroll", function () {
+                let scrollPercentage = ($(window).scrollTop() / ($(document).height() - $(window).height())) * 100;
+                let gradientPosition = (100 - (scrollPercentage % 100)) + "%";
+                
+                $(".empower-title").css("--gradient-position", gradientPosition);
+            });
+        });
+    </script>
 @endsection
