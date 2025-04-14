37e34ee6f1309d95f6599b2ca1098fabb9bac1cb
Muhammad Hamza <m.hamza7265@gmail.com>
2025-04-15 03:35:46 +0500

    migration fix

--- database/migrations/2025_04_14_203052_update_blogs_table_add_status_description_keywords.php
+++ database/migrations/2025_04_14_203052_update_blogs_table_add_status_description_keywords.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Blogs', function (Blueprint $table) {
+        Schema::table('blogs', function (Blueprint $table) {
             $table->string('status')->after('user_id');
             $table->text('meta_description')->after('status');
             $table->text('keywords')->after('status');
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Blogs', function (Blueprint $table) {
+        Schema::table('blogs', function (Blueprint $table) {
             $table->dropColumn('status');
             $table->dropColumn('meta_description');
             $table->dropColumn('keywords');
