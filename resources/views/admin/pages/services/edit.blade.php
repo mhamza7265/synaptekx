f100a86060af1a7402816ea58fe36d986ce27116
Muhammad Hamza <m.hamza7265@gmail.com>
2025-04-20 17:50:34 +0500

    added lfm in services edit page summernote

--- resources/views/admin/pages/services/edit.blade.php
+++ resources/views/admin/pages/services/edit.blade.php
@@ -367,6 +367,25 @@
                             deleteFile(target[0].src);
                         }
                     },
+                    buttons: {
+                        lfm: function (context) {
+                            var ui = $.summernote.ui;
+                            var button = ui.button({
+                                contents: '<i class="note-icon-picture"></i>',
+                                tooltip: 'Insert image with LFM',
+                                click: function () {
+                                    window.open('/laravel-filemanager?type=Images', 'lfm', 'width=900,height=600');
+                                    window.SetUrl = function (items) {
+                                        items.forEach(function (item) {
+                                            const html = '<p><img src="' + item.url + '" style="display:block;"></p><p><br></p>';
+                                            context.invoke('pasteHTML', html);
+                                        });
+                                    };
+                                }
+                            });
+                            return button.render();
+                        }
+                    }
                 });
             }
 
