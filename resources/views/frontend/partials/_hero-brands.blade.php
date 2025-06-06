66ad2c5e9693e9f360aeedc91a79c022a08ebb0d
Muhammad Hamza <m.hamza7265@gmail.com>
2025-06-07 00:57:19 +0500

    client loop animation continuety updated

--- resources/views/frontend/partials/_hero-brands.blade.php
+++ resources/views/frontend/partials/_hero-brands.blade.php
@@ -40,7 +40,7 @@
 
 <div class="brands-container slider-wrapper mt-5">
     <div class="slider-track">
-        @for ($i = 0; $i < 3; $i++)
+        {{-- @for ($i = 0; $i < 3; $i++)
             @foreach (data_get($page->sections, 'clients', []) as $key => $section)
                 <img src="{{ $section }}"
                     data-aos="fade-up"
@@ -49,6 +49,17 @@
                     data-aos-delay="{{ $loop->index }}00" 
                     class="carousel-partner-img"
                     />
+            @endforeach
+        @endfor --}}
+        @for ($i = 0; $i < 2; $i++) {{-- duplicate only once for seamless scroll --}}
+            @foreach (data_get($page->sections, 'clients', []) as $key => $section)
+                <img src="{{ $section }}"
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-easing="ease-in-out"
+                    data-aos-delay="{{ $loop->index }}00" 
+                    class="carousel-partner-img"
+                />
             @endforeach
         @endfor
         {{-- <img src="{{asset('images/frontend/aws_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" />
