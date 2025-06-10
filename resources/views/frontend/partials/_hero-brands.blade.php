b95dc405eb798e3e6a6f1c64887ed3951c50bd65
Muhammad Hamza <m.hamza7265@gmail.com>
2025-06-11 00:41:27 +0500

    cleaned up the hero brands file

--- resources/views/frontend/partials/_hero-brands.blade.php
+++ resources/views/frontend/partials/_hero-brands.blade.php
@@ -1,56 +1,5 @@
-{{-- <div class="brands-container">
-    <div class="d-flex justify-content-between align-items-center">
-        <img src="{{asset('images/frontend/aws_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" />
-        <img src="{{asset('images/frontend/hashicorp_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="100" />
-        <img src="{{asset('images/frontend/microsoft_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="200" />
-        <img src="{{asset('images/frontend/google_logo_1.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="300" />
-        <img src="{{asset('images/frontend/cisco_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="400" />
-        <img src="{{asset('images/frontend/equinix_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="500" />
-        <img src="{{asset('images/frontend/fortinet_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="600" />
-        <img src="{{asset('images/frontend/paloalto_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="700" />
-    </div>
-</div> --}}
-
-{{-- <div class="keen-slider auto-slider brands-container">
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/aws_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/hashicorp_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="100" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/microsoft_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="200" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/google_logo_1.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="300" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/cisco_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="400" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/equinix_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="500" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/fortinet_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="600" />
-    </div>
-    <div class="keen-slider__slide">
-        <img src="{{asset('images/frontend/paloalto_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="700" />
-    </div>
-</div> --}}
-
 <div class="brands-container slider-wrapper mt-5">
     <div class="slider-track">
-        {{-- @for ($i = 0; $i < 3; $i++)
-            @foreach (data_get($page->sections, 'clients', []) as $key => $section)
-                <img src="{{ $section }}"
-                    data-aos="fade-up"
-                    data-aos-duration="500"
-                    data-aos-easing="ease-in-out"
-                    data-aos-delay="{{ $loop->index }}00" 
-                    class="carousel-partner-img"
-                    />
-            @endforeach
-        @endfor --}}
         @for ($i = 0; $i < 2; $i++) {{-- duplicate only once for seamless scroll --}}
             @foreach (data_get($page->sections, 'clients', []) as $key => $section)
                 <img src="{{ $section }}"
@@ -62,22 +11,5 @@
                 />
             @endforeach
         @endfor
-        {{-- <img src="{{asset('images/frontend/aws_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" />
-        <img src="{{asset('images/frontend/hashicorp_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="100" />
-        <img src="{{asset('images/frontend/microsoft_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="200" />
-        <img src="{{asset('images/frontend/google_logo_1.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="300" />
-        <img src="{{asset('images/frontend/cisco_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="400" />
-        <img src="{{asset('images/frontend/equinix_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="500" />
-        <img src="{{asset('images/frontend/fortinet_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="600" />
-        <img src="{{asset('images/frontend/paloalto_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="700" /> --}}
-        {{-- duplicate --}}
-        {{-- <img src="{{asset('images/frontend/aws_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" />
-        <img src="{{asset('images/frontend/hashicorp_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="100" />
-        <img src="{{asset('images/frontend/microsoft_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="200" />
-        <img src="{{asset('images/frontend/google_logo_1.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="300" />
-        <img src="{{asset('images/frontend/cisco_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="400" />
-        <img src="{{asset('images/frontend/equinix_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="500" />
-        <img src="{{asset('images/frontend/fortinet_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="600" />
-        <img src="{{asset('images/frontend/paloalto_logo.svg')}}" data-aos="fade-up" data-aos-duration="500" data-aos-easing="ease-in-out" data-aos-delay="700" /> --}}
     </div>
 </div>
